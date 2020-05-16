c3c6c35d384565e19a53255f63a59d2ae7ec5fb4
Petr Tichy <peter@ihatetomatoes.net>
2020-05-17 09:28:59 +1000

    timeline completed

--- js/main.js
+++ js/main.js
@@ -1,44 +1,42 @@
-gsap.from(
-    'body', {
-        backgroundColor: "#fff", 
-        duration: 1.7, 
-        ease: 'none'
-    }
-);
-gsap.fromTo(
-    ['h1', '.intro'], {
-        opacity: 0, 
-        y: -20
-    }, 
-    {
-        opacity: 1, y: 0, 
-        duration: 0.6, 
-        ease: 'power1.out', 
-        delay: 1.5,
-        stagger: 0.2
-    }
-);
+const timeline = gsap.timeline({ 
+    duration: 1 ,
+    paused: true
+});
 
-gsap.from(
-    ['img', 'h2'], {
-        opacity: 0,
-        duration: 0.7, 
-        ease: 'none', 
-        delay: 2.8
-    }
-);
+timeline
+    .from(
+        'body', {
+            backgroundColor: "#fff", 
+            ease: 'none'
+        }
+    )
+    .fromTo(
+        ['h1', '.intro'], {
+            opacity: 0, 
+            y: -20
+        }, 
+        {
+            opacity: 1, y: 0, 
+            ease: 'power1.out',
+            stagger: 0.2
+        }
+    )
+    .from(
+        ['img', 'h2'], {
+            opacity: 0,
+            ease: 'none'
+        }
+    )
+    .fromTo(
+        'ul li',  {
+            opacity: 0, 
+            y: -20
+        }, 
+        {
+            opacity: 1, 
+            y: 0, 
+            ease: 'power1.out',
+            stagger: 0.2
+        }
 
-gsap.fromTo(
-    'ul li',  {
-        opacity: 0, 
-        y: -20
-    }, 
-    {
-        opacity: 1, 
-        y: 0, 
-        duration: 0.6, 
-        ease: 'power1.out',
-        stagger: 0.2, 
-        delay: 4
-    }
-);
+    );
