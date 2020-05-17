8ab3b4652ae9e7b89fc7cebf7e7cece7cf0e6cd7
Petr Tichy <peter@ihatetomatoes.net>
2020-05-17 15:26:29 +1000

    final

--- js/main.js
+++ js/main.js
@@ -11,8 +11,7 @@ const runComplete = () => {
 }
 
 const timeline = gsap.timeline({ 
-    duration: 1 ,
-    paused: true,
+    delay: 1,
     onStart: runStart,
     onComplete: runComplete,
     onUpdate: runUpdate
@@ -22,7 +21,8 @@ timeline
     .from(
         'body', {
             backgroundColor: "#fff", 
-            ease: 'none'
+            ease: 'none',
+            duration: 0.4
         }
     )
     .fromTo(
@@ -33,14 +33,18 @@ timeline
         {
             opacity: 1, y: 0, 
             ease: 'power1.out',
-            stagger: 0.2
-        }
+            stagger: 0.3,
+            duration: 0.9
+        },
+        '-=0.2'
     )
     .from(
         ['img', 'h2'], {
             opacity: 0,
-            ease: 'none'
-        }
+            ease: 'none',
+            duration: 0.5
+        },
+        '+=0.4'
     )
     .fromTo(
         'ul li',  {
@@ -49,59 +53,61 @@ timeline
         }, 
         {
             opacity: 1, 
+            duration: 0.3,
             y: 0, 
             ease: 'power1.out',
-            stagger: 0.2
-        }
+            stagger: 0.1
+        },
+        '+=0.3'
 
     );
 
 // const playButton = document.querySelector('#btnPlay');
-const playButton = document.getElementById('btnPlay');
-const btnPause = document.getElementById('btnPause');
-const btnResume = document.getElementById('btnResume');
-const btnReverse = document.getElementById('btnReverse');
-const btnSpeedUp = document.getElementById('btnSpeedUp');
-const btnSlowDown = document.getElementById('btnSlowDown');
-const btnSeek = document.getElementById('btnSeek');
-const btnProgress = document.getElementById('btnProgress');
-const btnRestart = document.getElementById('btnRestart');
+// const playButton = document.getElementById('btnPlay');
+// const btnPause = document.getElementById('btnPause');
+// const btnResume = document.getElementById('btnResume');
+// const btnReverse = document.getElementById('btnReverse');
+// const btnSpeedUp = document.getElementById('btnSpeedUp');
+// const btnSlowDown = document.getElementById('btnSlowDown');
+// const btnSeek = document.getElementById('btnSeek');
+// const btnProgress = document.getElementById('btnProgress');
+// const btnRestart = document.getElementById('btnRestart');
 
-playButton.addEventListener('click', () => {
-    timeline.play();
-});
+// playButton.addEventListener('click', () => {
+//     timeline.play();
+// });
 
-btnPause.addEventListener('click', () => {
-    timeline.pause();
-});
+// btnPause.addEventListener('click', () => {
+//     timeline.pause();
+// });
 
-btnResume.addEventListener('click', () => {
-    timeline.resume();
-});
+// btnResume.addEventListener('click', () => {
+//     timeline.resume();
+// });
 
-btnReverse.addEventListener('click', () => {
-    timeline.reverse();
-});
+// btnReverse.addEventListener('click', () => {
+//     timeline.reverse();
+// });
 
-btnSpeedUp.addEventListener('click', () => {
-    timeline.timeScale(2);
-});
+// btnSpeedUp.addEventListener('click', () => {
+//     timeline.timeScale(2);
+// });
 
-btnSlowDown.addEventListener('click', () => {
-    timeline.timeScale(0.5);
-});
+// btnSlowDown.addEventListener('click', () => {
+//     timeline.timeScale(0.5);
+// });
 
-console.log(timeline.duration());
-btnSeek.addEventListener('click', () => {
-    timeline.seek(1);
-});
+// console.log(timeline.duration());
+// btnSeek.addEventListener('click', () => {
+//     timeline.seek(1);
+// });
 
-btnProgress.addEventListener('click', () => {
-    timeline.progress(0.5);
-});
+// btnProgress.addEventListener('click', () => {
+//     timeline.progress(0.5);
+// });
 
-btnRestart.addEventListener('click', () => {
-    timeline.restart();
-});
+// btnRestart.addEventListener('click', () => {
+//     timeline.restart();
+// });
 
 
